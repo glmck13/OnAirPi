d35ad0513c3b393339d7c728dece308f915285be
glmck13 <glmck13@verizon.net>
2018-08-19 16:11:03 -0400

    Add files via upload

--- radioClient.sh
+++ radioClient.sh
@@ -1,10 +1,59 @@
 #!/bin/ksh
 
+WIFI_CONF=/var/www/html/wifi.conf
+WIFI_INFO=/var/www/html/wifi.info
+
+configWifi() {
+	[ -f "$WIFI_CONF" ] || return
+
+	set -A Conffile Network Password Command
+	n=0; while read ${Conffile[$n]}
+	do
+		let n=$n+1
+	done <$WIFI_CONF; rm -f $WIFI_CONF
+
+	unset Wifi; typeset -A Wifi
+	wpa_cli list_networks | sed -e "/^[0-9]/!d" | while read netid ssid x
+	do
+		Wifi[$ssid]=$netid
+	done
+
+	netid=${Wifi[$Network]}
+
+	case "$Command" in
+
+	Add)
+		if [ ! "$netid" ]; then
+			netid=$(wpa_cli add_network | grep -iv interface)
+			wpa_cli set_network $netid ssid \"$Network\"
+			wpa_cli set_network $netid psk \"$Password\"
+			wpa_cli set_network $netid scan_ssid 1
+			wpa_cli set_network $netid key_mgmt WPA-PSK
+			wpa_cli enable_network $netid
+			wpa_cli save_config
+		fi
+		;;
+
+	Delete)
+		if [ "$netid" ]; then
+			wpa_cli remove_network $netid
+			wpa_cli save_config
+		fi
+		;;
+	esac
+}
+
+showWifi() {
+	Network=$(iwconfig 2>&1 | grep SSID | cut -f2 -d:); print ${Network//\"/} >$WIFI_INFO
+	print $(wpa_cli list_networks | sed -e "/^[0-9]/!d" | cut -f2) >>$WIFI_INFO
+	print $(wpa_cli scan_results | sed -e "/:/!d" | cut -f5) >>$WIFI_INFO
+}
+
 trap : HUP INT QUIT
 
 exec >/dev/null 2>&1
 
-KeyStopCast=power KeyShutdown=media KeyStartCast=mute
+KeyStopCast=power KeyShutdown=media KeyStartCast=mute KeyLoadWifi=home
 
 LEDGREEN=4 LEDYELLOW=2 LEDRED=3
 
@@ -31,6 +80,7 @@ CLIENTCONF="$HTMLROOT/$(hostname)/ezstream.conf"
 
 while true
 do
+configWifi; showWifi
 scp -i $PEM $MCKLOGIN:$CLIENTCONF $EZCONF && break
 sleep 5
 done
@@ -86,6 +136,10 @@ do
 
 	$KeyShutdown)
 		sudo shutdown now
+		;;
+
+	$KeyLoadWifi)
+		configWifi; showWifi
 		;;
 
 	esac
